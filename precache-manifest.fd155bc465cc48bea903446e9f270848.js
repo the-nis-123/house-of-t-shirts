e55e07e13f012509e14f4dfc85690ded1519c2cc
the-nis-123 <vienseydenis@gmail.com>
2022-03-09 13:30:55 +0300

    Updates

---
+++ precache-manifest.fd155bc465cc48bea903446e9f270848.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ea94f920be1295f7d9c",
+    "url": "/house-of-t-shirts/static/css/main.48553ce5.chunk.css"
+  },
+  {
+    "revision": "6ea94f920be1295f7d9c",
+    "url": "/house-of-t-shirts/static/js/main.6ea94f92.chunk.js"
+  },
+  {
+    "revision": "3c4d52a22b061836d472",
+    "url": "/house-of-t-shirts/static/css/1.9f878b90.chunk.css"
+  },
+  {
+    "revision": "3c4d52a22b061836d472",
+    "url": "/house-of-t-shirts/static/js/1.3c4d52a2.chunk.js"
+  },
+  {
+    "revision": "fb9aa16b2177a5d8c170",
+    "url": "/house-of-t-shirts/static/js/runtime~main.fb9aa16b.js"
+  },
+  {
+    "revision": "a11e203100f445681a0142fa1977719f",
+    "url": "/house-of-t-shirts/static/media/logo.a11e2031.png"
+  },
+  {
+    "revision": "1bdbb5c64a1bf8a7260541aeb331cb94",
+    "url": "/house-of-t-shirts/index.html"
+  }
+];
